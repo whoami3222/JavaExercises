e2399e6814966c4b23f161d6eeb825a34d6217e5
David <dawid.szcz@gmail.com>
2024-02-22 21:15:00 +0200

    Nested loops

--- app.java
+++ app.java
@@ -139,9 +139,35 @@ public class app {
         scanner.close();
         System.out.println("Hello "+name);
         */
-
+/* 
         for(int i = 0; i <= 10; i++){
             System.out.println(i);
         }
+*/
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        String symbol = "";
+
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i = 1; i <= rows; i++){
+            System.out.println();
+            for(int j = 1; j <= columns; j++){
+
+                System.out.print(symbol);
+            }
+        }
+
+
+
+
+
     }
 }
