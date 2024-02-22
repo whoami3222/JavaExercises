b54fad9ac9d3e68f11285218976b8535fcb2a711
David <dawid.szcz@gmail.com>
2024-02-22 20:55:14 +0200

    Switch

--- app.java
+++ app.java
@@ -88,7 +88,7 @@ public class app {
         boolean z = random.nextBoolean();
         System.out.println(z);
         */
-
+/* 
         int age = 12;
 
         if(age>=75){
@@ -104,5 +104,27 @@ public class app {
         else{
             System.out.println("You are not an adult!");
         }
+         */
+
+         String day = "pizza";
+
+         switch(day){
+            case "Sunday": System.out.println("It is Sunday");
+            break;
+            case "Monday": System.out.println("It is Monday");
+            break;
+            case "Tuesday": System.out.println("It is Tuesday");
+            break;
+            case "Wednesday": System.out.println("It is Wednesday");
+            break;
+            case "Thursday": System.out.println("It is Thursday");
+            break;
+            case "Friday": System.out.println("It is Friday");
+            break;
+            case "Saturday": System.out.println("It is Saturday");
+            break;
+            default: System.out.println(day+" is not a day");
+            
+         }
     }
 }
