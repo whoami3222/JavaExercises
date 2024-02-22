a297118fb6e888071c26902ba377504e32e8fb74
David <dawid.szcz@gmail.com>
2024-02-22 21:56:20 +0200

    2D array

--- app.java
+++ app.java
@@ -171,7 +171,7 @@ public class app {
         //cars[0] = "Mustang";
 
        // System.out.println(cars[1]);
-
+/* 
        String[] cars = new String[3];
 
        cars[0] = "Camaro";
@@ -184,6 +184,30 @@ public class app {
         System.out.println(cars[i]);
        }
 
+*/
+
+       //2d array an array of array
+
+
+
+       String[][] cars = new String[3][3];
+
+       cars[0][0] = "Camaro";
+       cars[0][1] = "Corvette";
+       cars[0][2] = "Silverado";
+       cars[1][0] = "Mustang";
+       cars[1][1] = "BMW";
+       cars[1][2] = "Ranger";
+       cars[2][0] = "Mercedes";
+       cars[2][1] = "Ferrari";
+       cars[2][2] = "Audi";
+
+       for(int i=0; i<cars.length;i++){
+        System.out.println();
+        for(int j = 0; j<cars[i].length;j++){
+            System.out.print(cars[i][j]+" ");
+        }
+       }
 
 
 
