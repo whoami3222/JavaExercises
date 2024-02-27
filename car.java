d99cde620ebf30acc0429c4fdb7816643f9f0ca8
David <dawid.szcz@gmail.com>
2024-02-27 23:22:41 +0200

    toString Method

--- car.java
+++ car.java
@@ -1,15 +1,13 @@
 public class car {
+    
     String make = "BMW";
     String model = "F10";
     int year = 2012;
     String color = "black";
     double price =  12000.00;
 
-    void drive(){
-        System.out.println("You drive the car");
-    }
+    public String toString(){
 
-    void brake(){
-        System.out.println("You step on the brakes");
+        return make + "\n"+model+"\n"+color+"\n"+year;
     }
 }
