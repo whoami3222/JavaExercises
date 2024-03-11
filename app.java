ee10685ff7a580aceb993c6d43fdedd8a5b2e0ec
David <dawid.szcz@gmail.com>
2024-03-11 19:15:10 +0200

    generics1

--- app.java
+++ app.java
@@ -807,8 +807,67 @@ public class app {
 
     //JButton
 
-    new MiFrame();
+    //new MiFrame();
+    
+
+
+    //BorderLayout
+    
+
+    Integer[] intArray = {1,2,3,4,5};
+    Double[] doubleArray = {5.5,4.4,3.3,2.2,1.1};
+    Character[] charArray = {'H','E','L','L','O'};
+    String[] stringArray = {"B","Y","E"};
+
+    // displayArray(intArray);
+    // displayArray(stringArray);
+    // displayArray(charArray);
+    // displayArray(doubleArray);
+
+    System.out.println(getFirst(intArray));
+    System.out.println(getFirst(doubleArray));
+    System.out.println(getFirst(charArray));
+    System.out.println(getFirst(stringArray));
     
 
 }
+public static <Thing> void displayArray(Thing[] array) {
+    
+
+    for(Thing x: array){
+        System.out.println(x+" ");
+    }
+    System.out.println();
+}
+
+public static <T> T getFirst(T[] array){
+    return array[0];
+}
+
+// public static void displayArray(Double[] array) {
+    
+
+//     for(Double x: array){
+//         System.out.println(x+" ");
+//     }
+//     System.out.println();
+// }
+
+// public static void displayArray(Character[] array) {
+    
+
+//     for(Character x: array){
+//         System.out.println(x+" ");
+//     }
+//     System.out.println();
+// }
+
+// public static void displayArray(String[] array) {
+    
+
+//     for(String x: array){
+//         System.out.println(x+" ");
+//     }
+//     System.out.println();
+// }
 }
